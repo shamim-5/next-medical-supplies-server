a9edcd24e0c9834ce94c45e11c195f6a428ccc0c
shamim-5 <shamim.somadder5@gmail.com>
2024-05-13 12:35:34 +0600

    deploy on vercel a prisma project

--- dist/app/middlewares/globalErrorHandler.js
+++ dist/app/middlewares/globalErrorHandler.js
@@ -9,11 +9,10 @@ const handleValidationError_1 = __importDefault(require("../../errors/handleVali
 const zod_1 = require("zod");
 const handleClientError_1 = __importDefault(require("../../errors/handleClientError"));
 const handleZodError_1 = __importDefault(require("../../errors/handleZodError"));
-const logger_1 = require("../../shared/logger");
 const globalErrorHandler = (error, req, res, next) => {
     config_1.default.env === 'development'
         ? console.log(`🐱‍🏍 globalErrorHandler ~~`, { error })
-        : logger_1.errorlogger.error(`🐱‍🏍 globalErrorHandler ~~`, error);
+        : console.log(`🐱‍🏍 globalErrorHandler ~~`, error);
     let statusCode = 500;
     let message = 'Something went wrong !';
     let errorMessages = [];
